ac24d28413fb2ff3e7fb451cc015331960c59318
Shawn Hymers <39200454+shawnhymers@users.noreply.github.com>
2022-04-27 20:06:29 -0500

    yet another

--- src/components/app/blogs/vancouverActivityGuide.js
+++ src/components/app/blogs/vancouverActivityGuide.js
@@ -1,9 +1,29 @@
 import React from 'react';
 import { Container,Row} from 'react-bootstrap';
 import BlogHeader from "../elements/blogHeader";
+import  { useState,useEffect } from 'react';
 
 const VancouverActivityGuide = props => {
+  const[isMobile, setIsMobile]=useState(false)
 
+  useEffect(() => {
+    window.addEventListener('resize', updateDimensions);
+      updateDimensions()
+    window.addEventListener("contextmenu", e => e.preventDefault());
+
+    })
+
+  function updateDimensions() {
+    let windowWidth = typeof window !== "undefined" ? window.innerWidth : 0;
+    let windowHeight = typeof window !== "undefined" ? window.innerHeight : 0;
+    console.log('updating dimensions')
+    if (windowWidth>= windowHeight) {
+      setIsMobile(false)
+    }
+    else {
+      setIsMobile(true)
+    }
+  }
 
 return(
   <>
