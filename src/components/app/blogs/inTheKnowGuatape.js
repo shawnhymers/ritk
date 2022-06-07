e483cb26c88f283448f60adef80997d32414f7f7
Shawn Hymers <39200454+shawnhymers@users.noreply.github.com>
2022-06-07 16:17:43 -0500

    added paragraphs and pics itk guatape and banos guide w proofread text

--- src/components/app/blogs/inTheKnowGuatape.js
+++ src/components/app/blogs/inTheKnowGuatape.js
@@ -46,13 +46,26 @@ return(
 
                              {type:'header',text:"What To Do"},
                              {type:'listItem',text:'1) Climb El Peñol Rock'},
+                             {type:'paragraph',text:"El Penol Rock is a 65-million-year-old massive stone monolith that stands 200 meters above the valley below. It is an incredible sight to be seen from a distance, but it also provides amazing views from the top. You can climb the 708 steps built into the side of the cliff and come up to the viewing area, which is complete with gift shops, bars, and food stands. If you can plan your trip so you can experience sunset from the lookout, we highly recommend it."},
+
                              {type:'listItem',text:'2) Rent a Scooter'},
+                             {type:'paragraph',text:"The typical way to get El Penol rock is via one of the local rickshaw drivers. A scooter provides a second more scenic option. On a scooter you can take the long side of the ring road that connects the rock and the town of Guatape. It is a beautiful ride, and you get to see El Penol from every angle before you arrive. We rented a scooter from the hostel we were staying at and the whole rental and filling it back up with gas was less than $15."},
+                             {type:'verticalImage',src:'/GuatapeGallery/Guatape7.jpg'},
+
                              {type:'listItem',text:'3) Rent a Kayak'},
+                             {type:'paragraph',text:"Guatape is surrounded by a beautiful system of lakes. Although we didn’t have the weather for it when we were there, there are many companies that offer watercraft rentals. You can rent anything from a kayak to a jet ski. There is an unbelievable amount of water to explore, at whatever your preferred speed is."},
+                             {type:'horizontalImage',src:'/GuatapeGallery/Guatape9.jpg'},
+
                              {type:'listItem',text:'4) Wander The Town and Take Photos!'},
+                             {type:'paragraph',text:"Guatape is safe, small, and beautiful. The perfect place for a photo walk. Since a lot of people visit Guatape just for one day, early mornings before the first busses arrive, and in the evening as the last busses head back to Medellin, give you the best chance to avoid large crowds in the background."},
+                             {type:'diptych',src1:'/GuatapeGallery/Guatape21.jpg',src2:'/GuatapeGallery/Guatape22.jpg'},
 
                              {type:'header',text:"Where To Go For Vegan Options"},
                              {type:'listItem',text:'1) Namasté Vegan Food'},
+                             {type:'paragraph',text:"This unassuming little restaurant has as a small selection of burgers, salads, and wraps, along with smoothies. They also have a lovely rooftop patio that overlooks the quaint side street below."},
+
                              {type:'listItem',text:'2) Restaurante Vegano Zona Prosalud'},
+                             {type:'paragraph',text:"Centrally located, with a well-priced meal of the day and expansive menu. We barely scratched the surface of their large menu but were very impressed with the meal we had."},
 
                              {type:'paragraph',text:'Curious what it looks like through our eyes? See our photo gallery here. '}
 
