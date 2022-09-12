4f053c8cfadefdee313e03203d37e17f0dfab68c
Shawn Hymers <39200454+shawnhymers@users.noreply.github.com>
2022-09-12 20:53:00 +0200

    just pathname forGA

--- src/App.js
+++ src/App.js
@@ -70,7 +70,7 @@ ReactGA.initialize(TRACKING_ID);
 function App() {
 
   useEffect(() => {
-      ReactGA.pageview(window.location.pathname + window.location.search);
+      ReactGA.pageview(window.location.pathname);
     }, []);
   return (
     <>
